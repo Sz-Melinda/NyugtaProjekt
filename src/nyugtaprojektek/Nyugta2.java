a59b90b1f0ac3babd3997c4ab10aa188e325e5cb
Sz-Melinda <szikora.mleinda.24@gmail.com>
2022-03-23 11:46:04 +0100

    harmadik commit

--- src/nyugtaprojektek/Nyugta2.java
+++ src/nyugtaprojektek/Nyugta2.java
@@ -1,4 +1,5 @@
 package nyugtaprojektek;
+
 /* Probléma:
 a 3. tétel árát 650-ről átírtuk 1320-ra.
 Így szinte minden változott, mindenhez hozzá kell nyúlni
@@ -6,6 +7,7 @@ Refaktorizálás:
 anmit egynél többször használunk, 
 azt kiszervezzük változóba és erre hivatkozunk
 */
+
 public class Nyugta2 {
 
     public static void main(String[] args) {
@@ -33,36 +35,33 @@ public class Nyugta2 {
         int osszesen = tetel1 + tetel2 + tetel3;
         System.out.printf("Összesen:   %d %s\n", osszesen, huf);
         
-        //System.out.println("------------------");
+
         String szaggatottVonal = "-------------------";
         System.out.println(szaggatottVonal);
         
         int kedvMertek = 10;
         int kedvezmeny = osszesen / kedvMertek;
-        //System.out.println("Kedvezmény:  109 Ft");
+        
         System.out.printf("Kedvezmény:  %d %s\n", kedvezmeny, huf);
-        //System.out.println("(10%)");
+        
         System.out.printf("(%d%%)\n", kedvMertek);
         
-        //System.out.println("==================");
+        
         System.out.println(duplaVonal);
         
         int fizetendo = osszesen - kedvezmeny;
-        //System.out.println("Fizetendő:   981 Ft");
+        
         System.out.printf("Fizetendő:  %d %s\n", fizetendo, huf);
         double euro = fizetendo / 350.0;
-        huf = "\u20ac";//EZ NEM JÓ!!!!
+        huf = "\u20ac"; //EZ NEM JÓ!!!!
         System.out.printf("            %f %s\n", euro, huf);
         
         
-        //System.out.println("------------------");
+        
         System.out.println(szaggatottVonal);
         
         System.out.println("");
-        //System.out.print("_______");
-        //System.out.print("   ");
-        //System.out.println("_______");
-        
+       
         String rovidVonal = "_______";
         System.out.print(rovidVonal);
         String rovidVonalValaszto = "     ";
@@ -70,16 +69,16 @@ public class Nyugta2 {
         System.out.println(rovidVonal);
         
         System.out.print(" Dátum");
-        //System.out.print("   ");
+        
         System.out.print(rovidVonalValaszto);
         
         System.out.println("   Név");
         
-        //System.out.println("******************");
+        
         System.out.println(csillagok);
         
         System.out.println("        CÉG");
-        //System.out.println("******************");
+        
         System.out.println(csillagok);
     }
 
